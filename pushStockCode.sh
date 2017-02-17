7f96d884ff2169b7253308ac7fccb55899da792c
peter <hfutqiufei@163.com>
2017-02-17 10:12:36 +0800

    better

---
+++ pushStockCode.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# get into operation folder
+cd stockcode
+
+# get source file updates
+git pull
+
+
+# save change
+git add --all && git commit -a -m 'better' && git push
+
+# finally
+echo "所有股票代码已经更新。well done!"  
